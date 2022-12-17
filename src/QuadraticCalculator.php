89d9185015103256e228875463fc297eb3e468d0
Valdaria <Soulitor505@gmail.com>
2022-12-17 22:32:45 +0100

    Add Quadratic Calculator

---
+++ src/QuadraticCalculator.php
@@ -0,0 +1,30 @@
+<?php 
+class QuadraticCalculator {
+
+    public static function evaluate(string $quadraticFunction, int $x)
+    {
+        
+        // Parse a quadratic function
+        $re = '/^((?P<squaredValue>[+-]?\d+)x²)?((?P<xValue>[+-]?\d+)x)?((?P<oneValue>[+-]?\d+))?$/m';
+        
+        $x = 10;
+        
+        preg_match($re, $quadraticFunction, $matches);
+        
+        $result = 0;
+        
+        if ($squaredValue = $matches['squaredValue']) {
+            $result += intval($squaredValue)*$x*$x;
+        }
+        if ($xValue = $matches['xValue']) {
+            $result += intval($xValue)*$x;
+        }
+        if ($oneValue = $matches['oneValue']) {
+            $result += intval($oneValue);
+        }
+        
+        return $result;
+    }
+}
+
+?>
---
+++ tests/QuadraticCalculatorTest.php
@@ -0,0 +1,16 @@
+<?php 
+use PHPUnit\Framework\TestCase;
+
+final class QuadraticCalculatorTest extends TestCase {
+    public function testEvaluate() : void {
+        $this->assertEquals(
+            QuadraticCalculator::evaluate('200x²-69x-1000', 10),
+            18310
+        );
+
+        $this->assertEquals(
+            QuadraticCalculator::evaluate('2x²+2x+3', 10),
+            223
+        );
+    }
+}
